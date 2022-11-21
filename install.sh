d483c4e9fd8ff7a61f0dc2e9fa79cdf335f4652e
r4hul-cisin <rahul.vi@cisinlabs.com>
2022-11-21 17:02:32 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-INSTALLDIR="$HOME/.remhours"
+INSTALLDIR="$HOME/.remhours-test"
+bold=$(tput bold)
+normal=$(tput sgr0)
 
 mkdir $INSTALLDIR
-mv * $INSTALLDIR
+cp * $INSTALLDIR
 cd $INSTALLDIR
 sudo apt install -y python3-virtualenv python3.10
-virtualenv --python="/usr/bin/python3.10" env
+virtualenv --python="/usr/bin/python3" env
 $INSTALLDIR/env/bin/pip install -r requirements.txt
-echo "Enter Cisin email:"
-read UNAME
-echo "Enter password:"
-read -s PASSWD
-echo -e "$UNAME\n$PASSWD" >> $INSTALLDIR/config
-echo -e "alias remhours='$INSTALLDIR/env/bin/python main.py'" >> $HOME/.bashrc
-$INSTALLDIR/env/bin/python main.py
-echo "Installed successfully !"
-echo -e "\nRUN 'remhours' command to get the results.\nYou can edit '$INSTALLDIR/config' file to edit your cisin user login credentials.\n"
+
+if [ $? -eq 0 ]; then
+  echo "Enter Cisin email:"
+  read UNAME
+  echo "Enter password:"
+  read -s PASSWD
+  echo -e "$UNAME\n$PASSWD" >> $INSTALLDIR/config
+else
+  echo "Requirements not satisfied."
+  exit 1
+fi
+
+#echo -e "alias remhours='$INSTALLDIR/env/bin/python main.py'" >> $HOME/.bashrc
+$INSTALLDIR/env/bin/python3 main.py
+echo "${bold}Installed successfully !${normal}"
+echo -e "\nRun ${bold}remhours${normal} command to get the results.\nYou can edit ${bold}$INSTALLDIR/config${normal} file to edit your cisin user login credentials.\n"
